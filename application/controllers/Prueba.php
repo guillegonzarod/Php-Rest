4b15c0aa92924315ff18d396448152e102add631
Guille <info@guillegonzarod.com>
2019-03-16 15:16:52 +0100

    Crear verdaderos servicios REST a partir de nuestros controladores.

--- application/controllers/Prueba.php
+++ application/controllers/Prueba.php
@@ -1,18 +1,33 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Prueba extends CI_Controller
+// Añadimos la referencia para utilizar la clase 'REST_Controller'
+require_once(APPPATH . '/libraries/REST_Controller.php');
+use Restserver\Libraries\REST_Controller;
+
+// Heredamos de la clase 'REST_Controller':
+
+class Prueba extends REST_Controller
 {
     public function index($nombre)
     {
         echo "Hola $nombre!!!!";
     }
 
-    public function obtener_arreglo($index)
+    public function obtener_arreglo_get($index = 0)
     {
-        $arreglo = array("Manzana", "Pera", "Piña");
-
-        echo json_encode($arreglo[$index]);
+        // Comprobamos que el índice exista en el Array:
+        if ($index > 2) {
+            // En la respuesta añadimos el Código de Respuesta que creamos conveniente:
+            $respuesta = array('error' => true, 'mensaje' => 'No existe elemento con la posicion ' . $index);
+            // Le añadimos el Código de Error que creamos conveniente (podemos ver todos los Códigos de Respuesta en 'application/libraries/REST_Controller.php'):
+            $this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+            $arreglo = array("Manzana", "Pera", "Piña");
+            // Si el índice es correcto devolvemos el Código de Respuesta 'HTTP_OK = 200' con el recurso solicitado:
+            $respuesta = array('error' => false, 'fruta' => $arreglo[$index]);
+            $this->response($respuesta);
+        }
     }
 
     public function obtener_producto($codigo)
